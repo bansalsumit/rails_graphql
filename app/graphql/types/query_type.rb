01c9b5a026f8a9fe5bf2559d5001e311be22f662
Sumit Bansal <sbansal.mait@gmail.com>
2023-08-02 12:26:57 +0530

    Completed.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,7 +3,9 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
+    field :fetch_notes, resolver: Queries::FetchNotes
+    field :fetch_note, resolver: Queries::FetchNote
+
     field :test_field, String, null: false,
       description: "An example field added by the generator"
     def test_field
